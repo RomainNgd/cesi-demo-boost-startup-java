cabbfd532acd9007f1d0133a7c0ad84a1e558ce6
ROmain Nigond <romain.nigond@gmail.com>
2025-12-08 16:25:48 +0100

    test auto sync

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
     @GetMapping("/")
     public String hello() {
-        return "MANZAT --> Hello from Spring Boot 4 with Java 25!";
+        return "ROMAIN (PHP GOD) est beaucoup trop fort pour vous !";
     }
 
 }
